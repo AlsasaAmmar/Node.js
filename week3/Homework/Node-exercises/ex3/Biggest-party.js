61629f20adc0dfddc1b2d8561e411cb7f3fb3e58
Ammar <zarosho@gmail.com>
2020-03-14 18:38:21 +0100

    fixed mistakes

--- week3/Homework/Node-exercises/ex3/Biggest-party.js
+++ week3/Homework/Node-exercises/ex3/Biggest-party.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const PORT = 5000;
-const url = 'https://reservation100-sandbox.mxapps.io/rest-doc/api/swagger.json'
+const url = 'https://reservation100-sandbox.mxapps.io/api/reservations'
 
 const data = {
     "name" : "Ammar",
     "numberOfPeople": 2
 }
 
-const headers = { 'Content-Type' : 'application\json'}
+const headers = { 'Content-Type' : 'application/json'}
 
 let app = express();
 
 app.get('/', (req,res) => {
     fetch(url, { method: 'POST', headers: headers, body:  JSON.stringify(data)})
-    .then(res =>  console.log(res))
+    .then(res =>  res.text())
+    .then(data=> console.log(data))
     .catch(err => {
         console.log(err)
       })
