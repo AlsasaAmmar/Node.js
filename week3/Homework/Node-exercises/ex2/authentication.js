b0a15cef29927eeab36667f15cdb7e5f90899120
Ammar <zarosho@gmail.com>
2020-03-14 17:55:21 +0100

    fixed mistake

--- week3/Homework/Node-exercises/ex2/authentication.js
+++ week3/Homework/Node-exercises/ex2/authentication.js
@@ -1,12 +1,10 @@
-const express = require('express');
+
 const fetch = require('node-fetch');
 const url = 'https://restapiabasicauthe-sandbox.mxapps.io/api/books'
 const password = 'admin:hvgX8KlVEa';
 //const base46 = 'YWRtaW46aHZnWDhLbFZFYQ==';
-let buff = new Buffer(password);
+let buff = Buffer.from(password);
 let base64 = buff.toString('base64');
-
-let app = express();
 
 
 fetch(url, { headers: { 'Authorization': `Basic ${base64}` } })
